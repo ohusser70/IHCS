a58a2782ade0136b6157aca6daecbb531329c492
ohusser70 <ohusser@gmail.com>
2018-04-24 08:33:42 +0200

    premier commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+    ICHS   ICE HOCKEY CHAMPIONSHIP SIMULATOR
+
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!\n Welcome to ICHS v0.1" << endl;
+    return 0;
+}
